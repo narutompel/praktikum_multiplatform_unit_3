163273f61342ade1d470118684e9900dd788d6fd
narutompel <putujackson0@gmail.com>
2024-03-04 14:59:23 +0800

    Menggunakan Scaffold dan AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,26 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  
+}
+
+class HomePage extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ 
+ }
+} 
+
+class HomePage extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+ appBar: AppBar(
+ title: Text('Praktikum Layout Flutter'),
+ ),
+ body: // Isi dengan komponen layout selanjutnya
+ );
+ }
+}
