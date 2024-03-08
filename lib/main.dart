5c2d666c53aaf316f4d35ea920a83893ac4fccbd
narutompel <putujackson0@gmail.com>
2024-03-08 13:58:31 +0800

    menambahkan tampilan drawer menggunakan widget drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
 import 'package:flutter/widgets.dart';
@@ -25,7 +26,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         // header:
         appBar: AppBar(
-          title: Text('Unit 2: Profile Pengguna'),
+          title: Text('Unit 2: Profil Pengguna'),
           centerTitle: true,
           backgroundColor:
               Color.fromARGB(255, 217, 1, 255), // Ubah warna latar belakang
@@ -85,6 +86,19 @@ class HomePage extends StatelessWidget {
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Purus sit amet luctus venenatis lectus magna fringilla urna porttitor. Morbi quis commodo odio aenean. At imperdiet dui accumsan sit amet nulla. Turpis egestas maecenas pharetra convallis. Nulla at volutpat diam ut venenatis. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Vulputate eu scelerisque felis imperdiet."),
             )
           ],
-        ));
+        ),
+        drawer: _myDrawer(),
+        );
   }
+}
+
+Widget _myDrawer() {
+  return SizedBox(
+    child: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [],
+      ),
+    ),
+  );
 }
